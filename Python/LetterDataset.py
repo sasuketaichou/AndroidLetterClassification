621fb479964cf39b6ce4590e745fc226ff2dbb66
HEXA-AMIERULJAP\Hexa-Amierul.Japri <amierul.japri@hexainitiative.com>
2018-03-28 16:29:42 +0800

    Update LetterDataset

--- Python/LetterDataset.py
+++ Python/LetterDataset.py
@@ -1,14 +1,51 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import numpy as np
 from sklearn.model_selection import train_test_split
 from PIL import Image
 import os
+import sys
+import tarfile
+from six.moves import urllib
 
 class LetterDataset:
     def __init__(self):
+        
+        #download
+        def maybe_download_and_extract():
+
+            destination_dir = 'dataset'
+            URL = ' http://yaroslavvb.com/upload/notMNIST/notMNIST_small.tar.gz'
+
+            if not os.path.exists(destination_dir):
+                os.makedirs(destination_dir)
+
+            filename = URL.split('/')[-1]
+            filepath = os.path.join(destination_dir, filename)
+        
+            if not os.path.exists(filepath):
+                def _progress(count, block_size, total_size):
+                    sys.stdout.write('\r>> Downloading %s %.1f%%' % (filename,float(count * block_size) / float(total_size) * 100.0))
+                    sys.stdout.flush()
+                    
+                filepath, _ = urllib.request.urlretrieve(URL, filepath, _progress)
+                print()
+                statinfo = os.stat(filepath)
+                print('Successfully downloaded', filename, statinfo.st_size, 'bytes.')
+            
+            extracted_dir = os.path.join(destination_dir, 'notMNIST_small')
+            
+            if not os.path.exists(extracted_dir):
+                tarfile.open(filepath, 'r:gz').extractall(destination_dir)
+
+        #trigger
+        maybe_download_and_extract()       
         images, labels = [], []
 
         for i, letter in enumerate(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']):
-            directory = 'notMNIST_small/%s/' % letter
+            directory = 'dataset/notMNIST_small/%s/' % letter
             files = os.listdir(directory)
             label = np.array([0]*10)
             label[i] = 1
